e0270fe89a2cd5578eae1d2a4a77369e1ce2df4f
msboufanzi <mohameds.boufanzi@gmail.com>
2025-02-05 18:12:59 -0500

    final

---
+++ README.md
@@ -0,0 +1,57 @@
+# To Do List
+> In this project was built a simple list of To Do tasks. The user can add, edit, check completed and delete the tasks. This simple web page was built using webpack and served by a webpack dev server. The design followed this [template](https://web.archive.org/web/20180320194056/http://www.getminimalist.com:80/)
+
+
+![To_DO_List](https://meri-mg.github.io/To-Do-List/todo.png)
+
+# Built with
+- HTML5
+- CSS3
+- JavaScript
+- Webpack
+- Git and GitHub
+- VScode
+
+# Live Demo
+> The live Demo of the project is [here](https://meri-mg.github.io/To-Do-List/dist/)
+
+# Getting Started
+## Prerequisites:
+
+
+- Create an account on github
+
+- Install gitbash or use git on the command line.
+
+- Use an IDE of your preference such as VScode.
+
+## Steps to follow:
+
+- Choose a folder in your local machine where you want this repository to be copied
+
+- Clone this [repository](https://github.com/Meri-MG/To-Do-List-Project) to your locaL machine with `git clone` command.
+It should look similar to this git clone https://github.com/smithj/azure-docs.git Enter your github credentials if prompted.
+
+- On the command line, at the project's root, run 'npm install' to install app dependencies.
+
+- Next, run 'npm start' which will run the app in the development mode.
+
+- Open the index.html file with the browser of your preference.
+
+
+# 👤 Author 1 - Meri Gogichashvili
+- GitHub: [Meri-MG](https://github.com/Meri-MG) 
+- LinkedIn: [Meri Gogichashvili](https://www.linkedin.com/feed/)
+
+## 🤝 Contributing
+- Contributions, issues, and feature requests are welcome!
+- Feel free to check the [issues page](https://github.com/Meri-MG/To-Do-List-Project/issues).
+
+# Show your support
+Give a ⭐ if you like this project!
+
+# Acknowledgments
+Thanks to my teammates, learning and coding partners.
+
+# License
+This project is MIT licensed.
---
+++ src/index.js
@@ -0,0 +1,151 @@
+// import './style.css';
+import { completed, saveToStorage, getFromStorage } from './interactive.js';
+import { addTodoToList, deleteTodoList } from './add_remove.js';
+
+const listContainer = document.querySelector('.todo-lists');
+const form = document.querySelector('.input-container');
+const clearCompleted = document.querySelector('.btn');
+let todoLists = [];
+
+// check the order of the todo lists
+function checkOrder() {
+  todoLists.sort((a, b) => {
+    const index1 = a.id;
+    const index2 = b.id;
+
+    if (index1 < index2) {
+      return -1;
+    }
+    if (index1 > index2) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+// cleat previous elements in the container
+
+function clearElement(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
+
+function changeTask(e) {
+  const el = e.target;
+  const index = el.dataset.id;
+  const desc = el.innerText;
+
+  todoLists.forEach((task) => {
+    if (task.id == index) {
+      task.description = desc;
+    }
+  });
+  saveToStorage(todoLists);
+}
+
+function bindListenerTasks() {
+  const items = document.querySelectorAll('.items');
+  items.forEach((item) => {
+    item.removeEventListener('blur', (e) => {
+      changeTask(e);
+    });
+  });
+  items.forEach((item) => {
+    item.addEventListener('blur', (e) => {
+      changeTask(e);
+    });
+  });
+}
+
+// add todo lists to the object
+
+function addTodo() {
+  clearElement(listContainer);
+  todoLists.forEach((list) => {
+    const taskCompleted = list.completed ? 'linethrough' : '';
+    const checked = list.completed ? 'checked' : '';
+    listContainer.innerHTML += `<li class="list-container d-flex">
+    <input type="checkbox" class="checkbox" ${checked} >
+    <p class="items ${taskCompleted}" data-id="${list.id}" contenteditable="true">${list.description}</p>
+    <i class="fas fa-ellipsis-v dots"></i>
+    <i class="far fa-trash-alt delete"></i>
+    </li>`;
+  });
+
+  bindListenerTasks();
+}
+
+// UI of the project
+
+function displayTodo() {
+  checkOrder();
+  addTodo();
+  bindListenerTasks();
+}
+
+function reindex(todoLists) {
+  todoLists.forEach((todo, i) => {
+    todo.id = i;
+  });
+  saveToStorage(todoLists);
+  displayTodo();
+}
+
+// load the page
+
+window.addEventListener('DOMContentLoaded', () => {
+  if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
+    todoLists = getFromStorage();
+    displayTodo();
+  }
+  saveToStorage(todoLists);
+});
+
+// task completed
+
+listContainer.addEventListener('click', (e) => {
+  if (e.target.classList.contains('checkbox')) {
+    completed(e.target, todoLists);
+  }
+});
+
+// add the task
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todoInput = document.getElementById('myInput').value;
+  if (todoInput === '') {
+    alert('Please, enter the To Do for the day');
+  } else {
+    addTodoToList(todoInput, false, todoLists.length, todoLists);
+    addTodo();
+    saveToStorage(todoLists);
+    form.reset();
+  }
+});
+
+// delete the task
+
+listContainer.addEventListener('click', (e) => {
+  const task = document.querySelector('.items').textContent;
+  if (e.target.classList.contains('list-container')) {
+    deleteTodoList(e.target); // ui;
+  }
+  if (e.target.classList.contains('delete')) {
+    todoLists = todoLists.filter((item) => item.description !== task);
+    reindex(todoLists);
+    window.location.reload();
+  }
+});
+
+// clear all completed
+
+clearCompleted.addEventListener('click', () => {
+  todoLists = todoLists.filter((item) => item.completed === false);
+  reindex(todoLists);
+  saveToStorage(todoLists);
+  window.location.reload();
+});
+
+module.exports = { changeTask };
